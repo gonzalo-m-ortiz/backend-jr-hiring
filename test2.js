3a7684244304af357458cc013a0707031c4a299a
gonzalo-m-ortiz <ogonzalo1234@gmail.com>
2021-12-14 20:36:18 -0300

    fix: test2.js/Test2 correctly error handling.

--- test2.js
+++ test2.js
@@ -1,6 +1,20 @@
-const {  } = require('./test/utils/errors');
+const { ServerError, NotFound } = require('./test/utils/errors');
 
 module.exports = async function Test2 (server, queries) {
-    const response = await server.query(queries); // Will return an array with the results, empty array or error
-    return JSON.parse(response);
+    try {
+        const response = await server.query(queries); // Will return an array with the results, empty array or error
+        const responseParsed = JSON.parse(response);
+        
+        if (Array.isArray(responseParsed) && !responseParsed.length) {
+            return new NotFound(); // return NotFound error if array is empty
+        }
+
+        return responseParsed;
+    } catch (err) {
+        if (err instanceof ServerError) {
+            return err;
+        }
+        
+        return new ServerError(err.message);
+    }
 }
